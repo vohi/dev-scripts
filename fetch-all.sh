8cc98ecddf7783090b2bc040ec372fb919efc8ae
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-08-22 11:59:20 +0200

    Option to skip garbage collection for speedy fetching.

--- fetch-all.sh
+++ fetch-all.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
+RUN_GC=true
+case "$1" in
+  --fast)
+    RUN_GC=false
+esac
+
 for dir in */; do
   cd $dir
   echo "Updateing $dir..."
-  git gc --aggressive
+  if $RUN_GC
+  then
+    git gc --aggressive
+  fi
   git fetch --all
   echo
   cd ..
