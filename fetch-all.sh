ca3813fbdb26c4d0e56f532cd680416dfca8ec14
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-09-24 11:39:20 +0200

    Some additions to the fetcher.
    Make pruning and gc optional
    Add option to merge
    Some cleanups

--- fetch-all.sh
+++ fetch-all.sh
@@ -1,10 +1,54 @@
 #!/bin/bash
 
-RUN_GC=true
+RUN_GC=false
+RUN_PRUNE=false
+MERGE_FF=false
+PRINT_HELP=false
+
+while [[ $# > 1 ]]
+do
+
 case "$1" in
-  --fast)
-    RUN_GC=false
+  --gc)
+    RUN_GC=true
+    ;;
+
+  -f|--full)
+    RUN_GC=true
+    RUN_PRUNE=true
+    ;;
+
+  --prune)
+    RUN_PRUNE=true
+    ;;
+
+  -m|--merge)
+    MERGE_FF=true
+    shift
+    LOCAL_BRANCH=$1
+    shift
+    REMOTE_BRANCH=$1
+    ;;
+
+  *)
+    PRINT_HELP=true
+    ;;
 esac
+
+shift
+echo "first arg is now: $1"
+done
+
+if $PRINT_HELP
+then
+   echo "For all subdirectories, fetch all remotes"
+   echo "Options:"
+   echo "  -m, --merge LOCAL REMOTE: merge REMOTE into LOCAL (fast-forward only)"
+   echo "  --gc              : run git garbage collection"
+   echo "  --prune           : run git pruning"
+   echo "  -f, --full        : both gc and prune"
+   exit 0
+fi
 
 for dir in */; do
   cd $dir
@@ -13,7 +57,16 @@ for dir in */; do
   then
     git gc --aggressive
   fi
+  if $RUN_PRUNE
+  then
+    git prune
+  fi
   git fetch --all
+  if $MERGE_FF
+  then
+    git checkout $LOCAL_BRANCH
+    git merge $REMOTE_BRANCH --ff-only
+  fi
   echo
   cd ..
 done
