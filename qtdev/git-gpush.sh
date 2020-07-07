5b15c1de6303e8e99297d852f09d6838d829d184
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-07-07 13:33:10 +0200

    Make gpush operate on the clone rather the local worktree

--- qtdev/git-gpush.sh
+++ qtdev/git-gpush.sh
@@ -1,6 +1,7 @@
-sha=$1
+sha="$*"
 branch=$(git branch --show-current --no-color)
 remote="origin"
+repository=$(git worktree list | head -n 1 | awk '{print $1}')
 
 if [ -z "$1" ]
 then
@@ -19,6 +20,7 @@ error=0
 
 if [ "$count" -gt 1 ]
 then
+  cd $repository
   git checkout $remote/$branch 2> /dev/null
   git cherry-pick $sha
   error=$?
@@ -26,6 +28,8 @@ fi
 
 if [ "$error" -eq 0 ]
 then
+  echo "Pushing..."
+  git log --oneline $remote/$branch...
   git push gerrit HEAD:refs/for/$branch
 fi
 
